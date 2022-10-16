b7d713b875d6b5f53d287586f587c3dab102daea
Artashes Balabekyan <balabekyanartashes@gmail.com>
2022-10-17 02:04:32 +0400

    refactor: sfc: use infra/logutils for logging
    Signed-off-by: Artashes Balabekyan <balabekyanartashes@gmail.com>

--- src/sfc/pkg/metrics/controller.go
+++ src/sfc/pkg/metrics/controller.go
@@ -2,9 +2,9 @@ package metrics
 
 import (
 	"context"
-	"fmt"
 	"time"
 
+	log "gitlab.com/project-emco/core/emco-base/src/orchestrator/pkg/infra/logutils"
 	orchModule "gitlab.com/project-emco/core/emco-base/src/orchestrator/pkg/module"
 	"gitlab.com/project-emco/core/emco-base/src/sfc/pkg/module"
 )
@@ -13,33 +13,43 @@ func start() {
 	go func() {
 		orchClient := orchModule.NewClient()
 		client := module.NewClient()
+		fields := log.Fields{"service": "sfc"}
 		for {
 			projects, err := orchModule.NewProjectClient().GetAllProjects(context.Background())
 			if err != nil {
-				fmt.Println(err)
+				log.Error(err.Error(), fields)
 				continue
 			}
 
 			for _, proj := range projects {
+				fields := fields
+				fields["project"] = proj.MetaData.Name
+
 				apps, err := orchClient.CompositeApp.GetAllCompositeApps(context.Background(), proj.MetaData.Name)
 				if err != nil {
-					fmt.Println(err)
+					log.Error(err.Error(), fields)
 					continue
 				}
 				for _, app := range apps {
-
+					fields := fields
+					fields["composite_app"] = app.Metadata.Name
 					digs, err := orchClient.DeploymentIntentGroup.GetAllDeploymentIntentGroups(context.Background(), proj.MetaData.Name, app.Metadata.Name, app.Spec.Version)
 					if err != nil {
-						fmt.Println(err)
+						log.Error(err.Error(), fields)
 						continue
 					}
 					for _, dig := range digs {
+						fields := fields
+						fields["dig"] = dig.MetaData.Name
+
 						sfcs, err := client.SfcIntent.GetAllSfcIntents(proj.MetaData.Name, app.Metadata.Name, app.Spec.Version, dig.MetaData.Name)
 						if err != nil {
-							fmt.Println(err)
+							log.Error(err.Error(), fields)
 							continue
 						}
 						for _, sfc := range sfcs {
+							fields := fields
+							fields["sfc"] = sfc.Metadata.Name
 							SFCIntentGauge.WithLabelValues(
 								sfc.Metadata.Name,
 								proj.MetaData.Name, app.Metadata.Name, app.Spec.Version, dig.MetaData.Name,
@@ -48,7 +58,7 @@ func start() {
 							).Set(1)
 							links, err := client.SfcLinkIntent.GetAllSfcLinkIntents(proj.MetaData.Name, app.Metadata.Name, app.Spec.Version, dig.MetaData.Name, sfc.Metadata.Name)
 							if err != nil {
-								fmt.Println(err)
+								log.Error(err.Error(), fields)
 								continue
 							}
 							for _, link := range links {
