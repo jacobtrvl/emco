ef70067588b3baebd7614f89e1dd860c6b7f3067
Artashes Balabekyan <balabekyanartashes@gmail.com>
2022-10-17 02:17:38 +0400

    refactor: dcm: use infra/logutils for logging
    Signed-off-by: Artashes Balabekyan <balabekyanartashes@gmail.com>

--- src/dcm/pkg/metrics/controller.go
+++ src/dcm/pkg/metrics/controller.go
@@ -2,33 +2,39 @@ package metrics
 
 import (
 	"context"
-	"fmt"
 	"time"
 
 	"gitlab.com/project-emco/core/emco-base/src/dcm/pkg/module"
+	log "gitlab.com/project-emco/core/emco-base/src/orchestrator/pkg/infra/logutils"
 	orchModule "gitlab.com/project-emco/core/emco-base/src/orchestrator/pkg/module"
 )
 
 func start() {
 	go func() {
 		client := module.NewClient()
+		fields := log.Fields{"service": "dcm"}
 		for {
 			projects, err := orchModule.NewProjectClient().GetAllProjects(context.Background())
 			if err != nil {
-				fmt.Println(err)
+				log.Error(err.Error(), fields)
 				continue
 			}
 
 			for _, proj := range projects {
+				fields := fields
+				fields["project"] = proj.MetaData.Name
+
 				lcs, err := client.LogicalCloud.GetAll(context.Background(), proj.MetaData.Name)
 				if err != nil {
-					fmt.Println(err)
+					log.Error(err.Error(), fields)
 					continue
 				}
 				for _, lc := range lcs {
+					fields := fields
+					fields["logical_cloud"] = lc.MetaData.Name
 					st, err := client.LogicalCloud.GetState(context.Background(), proj.MetaData.Name, lc.MetaData.Name)
 					if err != nil {
-						fmt.Println("statusError", err)
+						log.Error(err.Error(), fields)
 					}
 					status := ""
 					if len(st.Actions) > 0 {
