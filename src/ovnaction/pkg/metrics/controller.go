bd2925090704dc4071aded35d32c0376be0ee1db
Artashes Balabekyan <balabekyanartashes@gmail.com>
2022-10-17 02:09:26 +0400

    refactor: ovnaction: use infra/logutils for logging
    Signed-off-by: Artashes Balabekyan <balabekyanartashes@gmail.com>

--- src/ovnaction/pkg/metrics/controller.go
+++ src/ovnaction/pkg/metrics/controller.go
@@ -2,9 +2,9 @@ package metrics
 
 import (
 	"context"
-	"fmt"
 	"time"
 
+	log "gitlab.com/project-emco/core/emco-base/src/orchestrator/pkg/infra/logutils"
 	orchModule "gitlab.com/project-emco/core/emco-base/src/orchestrator/pkg/module"
 	"gitlab.com/project-emco/core/emco-base/src/ovnaction/pkg/module"
 )
@@ -13,43 +13,57 @@ func start() {
 	go func() {
 		orchClient := orchModule.NewClient()
 		client := module.NewClient()
+		fields := log.Fields{"service": "ovnaction"}
 		for {
 			projects, err := orchModule.NewProjectClient().GetAllProjects(context.Background())
 			if err != nil {
-				fmt.Println(err)
+				log.Error(err.Error(), fields)
 				continue
 			}
 
 			for _, proj := range projects {
+				fields := fields
+				fields["project"] = proj.MetaData.Name
+
 				apps, err := orchClient.CompositeApp.GetAllCompositeApps(context.Background(), proj.MetaData.Name)
 				if err != nil {
-					fmt.Println(err)
+					log.Error(err.Error(), fields)
 					continue
 				}
 				for _, app := range apps {
+					fields := fields
+					fields["composite_app"] = app.Metadata.Name
+
 					digs, err := orchClient.DeploymentIntentGroup.GetAllDeploymentIntentGroups(context.Background(), proj.MetaData.Name, app.Metadata.Name, app.Spec.Version)
 					if err != nil {
-						fmt.Println(err)
+						log.Error(err.Error(), fields)
 						continue
 					}
 					for _, dig := range digs {
+						fields := fields
+						fields["dig"] = dig.MetaData.Name
+
 						ncis, err := client.NetControlIntent.GetNetControlIntents(proj.MetaData.Name, app.Metadata.Name, app.Spec.Version, dig.MetaData.Name)
 						if err != nil {
-							fmt.Println(err)
+							log.Error(err.Error(), fields)
 							continue
 						}
 						for _, nci := range ncis {
+							fields := fields
+							fields["net_controller_intent"] = nci.Metadata.Name
 							NetworkControllerIntentGauge.WithLabelValues(nci.Metadata.Name, proj.MetaData.Name, app.Metadata.Name, app.Spec.Version, dig.MetaData.Name).Set(1)
 							wis, err := client.WorkloadIntent.GetWorkloadIntents(proj.MetaData.Name, app.Metadata.Name, app.Spec.Version, dig.MetaData.Name, nci.Metadata.Name)
 							if err != nil {
-								fmt.Println(err)
+								log.Error(err.Error(), fields)
 								continue
 							}
 							for _, wi := range wis {
+								fields := fields
+								fields["workload_intent"] = wi.Metadata.Name
 								WorkloadIntentGauge.WithLabelValues(wi.Metadata.Name, proj.MetaData.Name, app.Metadata.Name, app.Spec.Version, dig.MetaData.Name, nci.Metadata.Name, wi.Spec.AppName, wi.Spec.WorkloadResource, wi.Spec.Type).Set(1)
 								wiifs, err := client.WorkloadIfIntent.GetWorkloadIfIntents(proj.MetaData.Name, app.Metadata.Name, app.Spec.Version, dig.MetaData.Name, nci.Metadata.Name, wi.Metadata.Name)
 								if err != nil {
-									fmt.Println(err)
+									log.Error(err.Error(), fields)
 									continue
 								}
 								for _, wiif := range wiifs {
