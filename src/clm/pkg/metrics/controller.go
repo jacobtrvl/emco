2bea43544fcf1b4305d063066e6b52785f169fd2
Artashes Balabekyan <balabekyanartashes@gmail.com>
2022-10-17 02:19:20 +0400

    refactor: clm: use infra/logutils for logging
    Signed-off-by: Artashes Balabekyan <balabekyanartashes@gmail.com>

--- src/clm/pkg/metrics/controller.go
+++ src/clm/pkg/metrics/controller.go
@@ -2,27 +2,30 @@ package metrics
 
 import (
 	"context"
-	"fmt"
 	"time"
 
 	"gitlab.com/project-emco/core/emco-base/src/clm/pkg/cluster"
+	log "gitlab.com/project-emco/core/emco-base/src/orchestrator/pkg/infra/logutils"
 )
 
 func start() {
 	go func() {
 		client := cluster.NewClusterClient()
+		fields := log.Fields{"service": "clm"}
 		for {
 			clps, err := client.GetClusterProviders(context.Background())
 			if err != nil {
-				fmt.Println(err)
+				log.Error(err.Error(), fields)
 				continue
 			}
 
 			for _, clp := range clps {
+				fields := fields
+				fields["cluster_provider"] = clp.Metadata.Name
 				CLPGauge.WithLabelValues(clp.Metadata.Name).Set(1)
 				clusters, err := client.GetClusters(context.Background(), clp.Metadata.Name)
 				if err != nil {
-					fmt.Println(err)
+					log.Error(err.Error(), fields)
 					continue
 				}
 				for _, cl := range clusters {
