6891ba6168dcf59c7751700c3141c8c1bff56de3
Artashes Balabekyan <balabekyanartashes@gmail.com>
2022-10-17 02:15:17 +0400

    refactor: dtc: use infra/logutils for logging
    Signed-off-by: Artashes Balabekyan <balabekyanartashes@gmail.com>

--- src/dtc/pkg/metrics/controller.go
+++ src/dtc/pkg/metrics/controller.go
@@ -7,6 +7,7 @@ import (
 	"time"
 
 	"gitlab.com/project-emco/core/emco-base/src/dtc/pkg/module"
+	log "gitlab.com/project-emco/core/emco-base/src/orchestrator/pkg/infra/logutils"
 	orchModule "gitlab.com/project-emco/core/emco-base/src/orchestrator/pkg/module"
 )
 
@@ -14,40 +15,52 @@ func start() {
 	go func() {
 		orchClient := orchModule.NewClient()
 		client := module.NewClient()
+		fields := log.Fields{"service": "dtc"}
 		for {
 			projects, err := orchModule.NewProjectClient().GetAllProjects(context.Background())
 			if err != nil {
-				fmt.Println(err)
+				log.Error(err.Error(), fields)
 				continue
 			}
 
 			for _, proj := range projects {
+				fields := fields
+				fields["project"] = proj.MetaData.Name
 				apps, err := orchClient.CompositeApp.GetAllCompositeApps(context.Background(), proj.MetaData.Name)
 				if err != nil {
-					fmt.Println(err)
+					log.Error(err.Error(), fields)
 					continue
 				}
 				for _, app := range apps {
+					fields := fields
+					fields["composite_app"] = app.Metadata.Name
 
 					digs, err := orchClient.DeploymentIntentGroup.GetAllDeploymentIntentGroups(context.Background(), proj.MetaData.Name, app.Metadata.Name, app.Spec.Version)
 					if err != nil {
-						fmt.Println(err)
+						log.Error(err.Error(), fields)
 						continue
 					}
 					for _, dig := range digs {
+						fields := fields
+						fields["dig"] = dig.MetaData.Name
+
 						tgis, err := client.TrafficGroupIntent.GetTrafficGroupIntents(proj.MetaData.Name, app.Metadata.Name, app.Spec.Version, dig.MetaData.Name)
 						if err != nil {
-							fmt.Println(err)
+							log.Error(err.Error(), fields)
 							continue
 						}
 						for _, tgi := range tgis {
+							fields := fields
+							fields["traffic_group_intent"] = tgi.Metadata.Name
 							TrafficGroupIntentGauge.WithLabelValues(tgi.Metadata.Name, proj.MetaData.Name, app.Metadata.Name, app.Spec.Version, dig.MetaData.Name).Set(1)
 							iis, err := client.ServerInboundIntent.GetServerInboundIntents(proj.MetaData.Name, app.Metadata.Name, app.Spec.Version, dig.MetaData.Name, tgi.Metadata.Name)
 							if err != nil {
-								fmt.Println(err)
+								log.Error(err.Error(), fields)
 								continue
 							}
 							for _, ii := range iis {
+								fields := fields
+								fields["server_inbound_intent"] = ii.Metadata.Name
 								InboundIntentGauge.WithLabelValues(
 									ii.Metadata.Name,
 									proj.MetaData.Name, app.Metadata.Name, app.Spec.Version, dig.MetaData.Name, tgi.Metadata.Name,
@@ -64,10 +77,12 @@ func start() {
 								).Set(1)
 								iics, err := client.ClientsInboundIntent.GetClientsInboundIntents(proj.MetaData.Name, app.Metadata.Name, app.Spec.Version, dig.MetaData.Name, tgi.Metadata.Name, ii.Metadata.Name)
 								if err != nil {
-									fmt.Println(err)
+									log.Error(err.Error(), fields)
 									continue
 								}
 								for _, iic := range iics {
+									fields := fields
+									fields["inbound_client_intent"] = iic.Metadata.Name
 									InboundIntentClientGauge.WithLabelValues(
 										iic.Metadata.Name,
 										proj.MetaData.Name, app.Metadata.Name, app.Spec.Version, dig.MetaData.Name, tgi.Metadata.Name, ii.Metadata.Name,
@@ -77,7 +92,7 @@ func start() {
 									).Set(1)
 									aps, err := client.ClientsAccessInboundIntent.GetClientsAccessInboundIntents(proj.MetaData.Name, app.Metadata.Name, app.Spec.Version, dig.MetaData.Name, tgi.Metadata.Name, ii.Metadata.Name, iic.Metadata.Name)
 									if err != nil {
-										fmt.Println(err)
+										log.Error(err.Error(), fields)
 										continue
 									}
 									for _, ap := range aps {
